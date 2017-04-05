b7cb01c0de6b725ffaef18277a6e549b0a25b32a
Dinacode <dinacode@MacBook-Pro-de-Pablo.local>
2017-04-06 00:11:44 +0200

    Removed OddEven implementation

--- src/agiletdd/oddeven/OddEven.java
+++ src/agiletdd/oddeven/OddEven.java
@@ -3,18 +3,9 @@ package agiletdd.oddeven;
 public class OddEven {
 
 	public String print(int number){
-		if(number > 100)
-			throw new RuntimeException(number + " is greater than 100");
 		
-		if (number == 2) return "PRIME";
-		if (number % 2 == 0) return "EVEN";
-		 
-		for(int i=3 ;i*i <= number ;i += 2) {
-			if(number % i==0)
-				return "ODD";
-		}
-		 
-		return "PRIME";
+		return null;
+		
 	}
 	
 }
